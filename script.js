83db8a7d8814a122495c6ae19d0d1c3c7c2f5afe
0xdavidel <0xdavidel@gmail.com>
2021-07-09 20:17:22 +0200

    fixed?

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
+  const total = Date.parse(endtime) - Date.parse(new Date().toISOString());
   const seconds = Math.floor((total / 1000) % 60);
   const minutes = Math.floor((total / 1000 / 60) % 60);
   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
