673be71019ece5ca3158e814b5150fe6d0cb0866
0xdavidel <0xdavidel@gmail.com>
2021-07-10 00:45:57 +0300

    rick roll

--- rotten_eggs.js
+++ rotten_eggs.js
@@ -1,4 +1,6 @@
 // Why are you looking here???? (DM UserDave#1337 with the phrase rotten_eggs for ego boosting)
+// No prompt here
+
 
 function randomChoice(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
@@ -14,14 +16,44 @@ var possible_prompts = ["Welcome to gamejam",
     "Good luck and have fun",
     "A bug is a feature"]
 
+
+
 var prompt_element = document.getElementById("random_prompt");
 prompt_element.innerHTML = randomChoice(possible_prompts)
 
-var rot = 360;
 
+function handleClicks(count)
+{
+    console.log(click_count);
+    var prompt_element = document.getElementById("random_prompt");
+    if(click_count  === 1)
+    {
+        prompt_element.innerHTM= "Keep clicking and you will get the prompt!";
+    }
+    if(click_count === 3)
+    {
+        prompt_element.innerHTML="Keep going!";
+    }
+    if(click_count === 5)
+    {
+        prompt_element.innerHTML="You must really want to see the prompt...";
+    }
+    if(click_count === 8)
+    {
+        prompt_element.innerHTML="So close!!!";
+    }
+    if(click_count === 10)
+    {
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    }
+}
+
+var click_count = 0
+var rot = 360;
 title_spinner = document.getElementById("title_thingy")
 title_spinner.addEventListener("click", function() {
-    console.log("click")
+    click_count += 1;
+    handleClicks(click_count);
     title_spinner.style = 'transform: rotate(' + rot + 'deg)';
   rot += 360;
 });
