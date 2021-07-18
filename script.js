e2f8c5747aa2689e62e651712bfcd2daedc58884
0xdavidel <0xdavidel@gmail.com>
2021-07-18 17:11:48 +0300

    GameJam end additions + favicon

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
+function annouce_end()
+{
+  var clock = document.getElementById("clockdiv");
+  clock.innerHTML= "";
+  var title = document.getElementById("title")
+  title.innerHTML = '<img src="./GameJamLogo-100.svg" style="height: 2em; padding-right:0.2em ;">';
+  title.innerHTML += 'GAME JAM HAS ENDED!';
+  title.innerHTML += '<img src="./GameJamLogo-100.svg" style="height: 2em; padding-right:0.2em ;">';
+  var submit_button = document.getElementById("submit_button");
+  submit_button.innerHTML = "";
+}
+
 function getTimeRemaining(endtime) {
   var d = new Date();
 
   var localTime = d.getTime();
 
-  var localOffset = 0;// d.getTimezoneOffset() * 60000;
+  var localOffset = 0;
 
   var utc = localTime + localOffset;
 
   var current = new Date(utc)
-  //console.log("current : " + current +"\ndeadline : "+ deadline)
   const total = Date.parse(endtime) - Date.parse(current);
   const seconds = Math.floor((total / 1000) % 60);
   const minutes = Math.floor((total / 1000 / 60) % 60);
   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
   const days = Math.floor(total / (1000 * 60 * 60 * 24));
   
+  if (seconds < 0)
+  {
+    annouce_end();
+  }
   return {
     total,
     days,
@@ -48,5 +63,13 @@ function initializeClock(id, endtime) {
   const timeinterval = setInterval(updateClock, 1000);
 }
 
-const deadline = new Date(Date.UTC(2021,6,18,20,0,0));
-initializeClock('clockdiv', deadline);
+var deadline = new Date(Date.UTC(2021,6,18,20,0,0));
+var seconds_left = getTimeRemaining(deadline).seconds;
+
+if(seconds_left > 0)
+{
+  initializeClock('clockdiv', deadline);
+}
+else{
+  annouce_end();
+}
